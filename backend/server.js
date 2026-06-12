31bb61f75bf5ece62808d465b68dcd24a879e3b8
adil2133-git <adilaadi340@gmail.com>
2026-06-12 14:33:18 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const connectDB = require("./config/db");
+const {connectRedis} = require("./config/redis")
+require("dotenv").config()
+
+connectDB()
+connectRedis()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+});
