93c687287a05fca9ca0139a0eae918a7126fec23
adil2133-git <adilaadi340@gmail.com>
2026-06-13 16:06:44 +0530

    otpverificationmodal path fix in customer registration page

--- frontend/src/auth/customerRegistration.tsx
+++ frontend/src/auth/customerRegistration.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import OtpVerificationModal from "./OtpVerificationModal";
+import OtpVerificationModal from "./otpVerificationModal";
 
 export default function CustomerRegistration() {
   const [showPassword, setShowPassword] = useState(false);
