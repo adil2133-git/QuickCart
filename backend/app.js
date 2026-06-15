f9527f1667d147971df5eb50899c884745fe3887
adil2133-git <adilaadi340@gmail.com>
2026-06-15 15:00:57 +0530

    cors setup done

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
 app.use(express.json())
 
 const customerAuthRoutes = require("./routes/customer/authRoutes")
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+)
 
 app.use("/api/auth", customerAuthRoutes)
 
