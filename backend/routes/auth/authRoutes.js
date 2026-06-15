2ba7b638ac73df8fdc8486b1fddf07b3bc963eb4
adil2133-git <adilaadi340@gmail.com>
2026-06-15 17:00:48 +0530

    login route setup done

--- backend/routes/auth/authRoutes.js
+++ backend/routes/auth/authRoutes.js
@@ -1,14 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
-const {
-    CustomerRegister
-} = require("../../controllers/auth/registrationController")
-
-const {verifyOtpController, resendOTPController} = require("../../controllers/auth/otpController")
+const { CustomerRegister } = require("../../controllers/auth/registrationController")
+const { verifyOtpController, resendOTPController } = require("../../controllers/auth/otpController")
+const { Login } = require("../../controllers/auth/loginController")
 
 router.post("/register", CustomerRegister);
 router.post("/register/verify-otp", verifyOtpController);
 router.post("/register/resend-otp", resendOTPController);
+router.post("/login",)
 
 module.exports = router
