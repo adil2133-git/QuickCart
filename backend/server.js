0d4a12fb5165c13b8cf783a6e7734529411e62e2
adil2133-git <adilaadi340@gmail.com>
2026-06-30 17:04:06 +0530

    server.js updated with the socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 require("dotenv").config()
 
+const http = require("http");
 const app = require("./app")
 const connectDB = require("./config/db");
 const {connectRedis} = require("./config/redis")
+const { initSocket } = require("./socket");
 
 connectDB()
 connectRedis()
 
-app.listen(process.env.PORT, () => {
+const httpServer = http.createServer(app);
+initSocket(httpServer);
+
+httpServer.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 });
